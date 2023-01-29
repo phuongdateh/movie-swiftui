060777ec8bef67127e444fc202edc8678d56da57
james.doan <james.doan@zillearn.com>
2023-01-29 09:26:46 +0700

    first commit

---
+++ Movie/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Movie
+//
+//  Created by James on 28/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieList()
+    }
+}
+
+struct MovieList: View {
+    var body: some View {
+        Text("asdjhasjdhsd")
+    }
+}
---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by James on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieList()
+        }
+    }
+}
